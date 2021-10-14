2aca656c251abfdaf736ecd2cce2f0cc3850ae49
Peti <Peti@DESKTOP-SHEQHLK>
2021-10-14 16:24:05 +0200

    commit internal

---
+++ minta_zh/minta_zh/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace minta_zh
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cboxYears = new System.Windows.Forms.ComboBox();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cboxYears
+            // 
+            this.cboxYears.FormattingEnabled = true;
+            this.cboxYears.Location = new System.Drawing.Point(12, 23);
+            this.cboxYears.Name = "cboxYears";
+            this.cboxYears.Size = new System.Drawing.Size(196, 21);
+            this.cboxYears.TabIndex = 0;
+            this.cboxYears.SelectedIndexChanged += new System.EventHandler(this.CreateYearFilter_SelectedIndexChanged);
+            // 
+            // btnExport
+            // 
+            this.btnExport.Location = new System.Drawing.Point(214, 21);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(102, 23);
+            this.btnExport.TabIndex = 1;
+            this.btnExport.Text = "Export";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(328, 56);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.cboxYears);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cboxYears;
+        private System.Windows.Forms.Button btnExport;
+    }
+}
+
