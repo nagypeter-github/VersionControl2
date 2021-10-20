32ffa2d70a36e226c3d9f831debe976870e47ca1
Peti <Peti@DESKTOP-SHEQHLK>
2021-10-20 14:33:21 +0200

    commit

--- excel_export/UserMaintenance/Form1.cs
+++ excel_export/UserMaintenance/Form1.cs
@@ -10,13 +10,14 @@ using System.Windows.Forms;
 using Excel = Microsoft.Office.Interop.Excel;
 using System.Reflection;
 using System.Security.Cryptography.X509Certificates;
+using UserMaintenance.Entities;
 
 namespace UserMaintenance
 {
     public partial class Form1 : Form
     {
-       
-       
+
+        BindingList<User> users = new BindingList<User>();
         public Form1()
         {
            
@@ -25,12 +26,21 @@ namespace UserMaintenance
             lblFirstName.Text = Resource1.FirstName; // label2
             btnAdd.Text = Resource1.Add; // button1
 
-
+            listUsers.DataSource = users;
+            listUsers.ValueMember = "ID";
+            listUsers.DisplayMember = "FullName";
 
         }
 
-       
-        
-
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            var u = new User()
+            {
+                LastName = txtLastName.Text,
+                FirstName = txtFirstName.Text
+            };
+        }
     }
+
+    
 }
